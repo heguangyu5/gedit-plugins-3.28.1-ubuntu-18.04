c3468bc33b9498c0a066eff9ac30b377745e59a8
heguangyu5 <heguangyu5@qq.com>
2019-09-12 14:56:51 +0800

    original gedit-plugins-3.28.1 from https://download.gnome.org/sources/gedit-plugins/3.28/

---
+++ plugins/gpdefs.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+VERSION = "3.28.1"
+PACKAGE = "gedit-plugins"
+PACKAGE_STRING = "gedit-plugins 3.28.1"
+GETTEXT_PACKAGE = "gedit-plugins"
+GP_LOCALEDIR = "/usr/share/locale"
